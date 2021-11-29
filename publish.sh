6be6d9a4bc3ea2904dc38759bb739b5b567d96c0
Julien Lacour <52037308+jlacour31@users.noreply.github.com>
2021-11-29 13:33:23 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@ echo -e "$LICENSE_KEY" > ope/licensekey.txt
 # Replace all the '@@' characters back to '\n' in the 'licensekey.txt' file
 sed -i 's/@@/\n/g' ope/licensekey.txt
 # Run the PDF CSS HTML5 transformation
-ope/bin/dita -f pdf-css-html5 -i sample/space.ditamap -o sample/out/pdf-css-html5
+ope/bin/dita -f pdf-css-html5 -i sample/space.ditamap -o sample/out/pdf-css-html5 -v
 # Run the WebHelp Responsive transformation
-ope/bin/dita -f webhelp-responsive -i sample/space.ditamap -o sample/out/webhelp-responsive
+ope/bin/dita -f webhelp-responsive -i sample/space.ditamap -o sample/out/webhelp-responsive -v
